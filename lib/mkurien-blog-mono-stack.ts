ea26bd76bed6bb9a65eaff290da6c092e0008928
maku <manojasho@gmail.com>
2021-09-18 15:29:43 +0100

    Add cert for domain. Update dependencies

--- lib/mkurien-blog-mono-stack.ts
+++ lib/mkurien-blog-mono-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import * as acm from '@aws-cdk/aws-certificatemanager';
 import { LikeCounter } from './likecounter';
 
 export class MkurienBlogMonoStack extends cdk.Stack {
@@ -9,10 +10,20 @@ export class MkurienBlogMonoStack extends cdk.Stack {
     const likeCounter = new LikeCounter(this, 'LikeCounter', {
     });
 
-    const api = new apigw.RestApi(this, 'blog-api');
+    const api_mkurien_dot_com = 'api.mkurien.com';
+    const certForMkuriencom = new acm.Certificate(this, "cert-mkurien.com", {
+      domainName: api_mkurien_dot_com,
+    });
+    // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-apigateway-readme.html
+    const api = new apigw.RestApi(this, api_mkurien_dot_com, {
+      domainName: {
+        domainName: api_mkurien_dot_com,
+        certificate: certForMkuriencom
+      }
+    });
     api.root.addMethod('ANY');
-
     const likes = api.root.addResource('likes');
-    likes.addMethod('GET',  new apigw.LambdaIntegration(likeCounter.getLikesHandler));
-    likes.addMethod('POST', new apigw.LambdaIntegration(likeCounter.incrementLikesHandler));  }
+    likes.addMethod('GET', new apigw.LambdaIntegration(likeCounter.getLikesHandler));
+    likes.addMethod('POST', new apigw.LambdaIntegration(likeCounter.incrementLikesHandler));
+  }
 }
