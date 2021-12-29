47f792ebd75abff4b32476c532147f99e6d8703e
maku <manojasho@gmail.com>
2021-12-29 17:36:56 +0000

    Add integration test for feedback

--- integration_test/test_rest_endpoints.py
+++ integration_test/test_rest_endpoints.py
@@ -1,15 +1,30 @@
 import requests
+import json
 
-def test_should_get_likes_for_article():
-    r = requests.get('https://api.mkurien.com/likes?articleId=3')
-    assert r.status_code == 200
-    assert r.json() >= 1
+TEST_ARTICLE_ID=0
+
+# def test_should_get_likes_for_article():
+#     r = requests.get(f'https://api.mkurien.com/likes?articleId={TEST_ARTICLE_ID}')
+#     assert r.status_code == 200
+#     assert r.json() >= 1
     
-def test_should_increment_likes_for_article():
-    initial_likes = requests.get('https://api.mkurien.com/likes?articleId=3').json()
+# def test_should_increment_likes_for_article():
+#     initial_likes = requests.get(f'https://api.mkurien.com/likes?articleId={TEST_ARTICLE_ID}').json()
 
-    r = requests.post('https://api.mkurien.com/likes?articleId=3')
+#     r = requests.post(f'https://api.mkurien.com/likes?articleId={TEST_ARTICLE_ID}')
 
+#     assert r.status_code == 200
+#     assert r.json() > initial_likes
+
+def test_should_create_feedback_for_article():
+    r = requests.put(f'https://api.mkurien.com/feedback?articleId={TEST_ARTICLE_ID}', 
+        headers= {
+            'Accept': "application/json",
+            "Content-Type": "application/json",
+        },
+        data= json.dumps({
+            'feedback': 'blah',
+            'twitterHandle': '@blah',
+            'email': 'blah@blah.com'
+            }))
     assert r.status_code == 200
-    assert r.json() > initial_likes
-
